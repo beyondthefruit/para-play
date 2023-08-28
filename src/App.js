6a4c89240c30de246dc30c5e4cb2a6cd9af50e43
Kevin Hanard <kevin.hanard@gmail.com>
2023-08-28 14:03:37 +1000

    feat: flip cards

--- src/App.js
+++ src/App.js
@@ -1,78 +1,160 @@
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-function App() {
-  return (
-    <div style={{ width: '100%', height: '100%', background: 'black' }}>
-      <Parallax pages={3}>
-        <ParallaxLayer
-          offset={0}
-          speed={1}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <h1 style={{ width: '100%', height: '40%', background: 'pink' }}>
-            Hello World
-          </h1>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={0.1} style={{}}>
-          <h1 style={{ width: '100%', height: '40%', background: 'grey' }}>
-            Mini moy
-          </h1>
-        </ParallaxLayer>
+import { useState } from 'react';
+import catCrew from './data.js';
+import Crew from './Components/crew';
 
-        <ParallaxLayer
-          offset={2}
-          speed={1}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-          }}
-        >
-          <div
+function App() {
+  const alignCenter = {
+    display: 'flex',
+    alignItems: 'center',
+  };
+  const [flip, setFlip] = useState(false);
+  const [dataCrew] = useState(catCrew);
+  console.log(dataCrew);
+  console.log(catCrew);
+  return (
+    <>
+      <div className='background'>
+        <Parallax pages={5}>
+          <ParallaxLayer
+            offset={0}
+            speed={0.8}
             style={{
-              display: 'flex',
+              alignCenter,
               justifyContent: 'center',
-              alignItems: 'center',
-              textAlign: 'center',
-              margin: '0 5%',
-              width: '40%',
-              height: '60%',
-              background: 'orange',
             }}
           >
-            <h1>Bernard</h1>
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer
-          offset={2}
-          speed={20}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-          }}
-        >
-          <div
+            <img
+              src={require('./imgs/rocket-flying-through-space.jpg')}
+              style={{ display: 'block', width: '100%', height: '200%' }}
+            />
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={1}
+            speed={1}
             style={{
-              display: 'flex',
+              alignCenter,
               justifyContent: 'center',
-              alignItems: 'center',
-              textAlign: 'center',
-              margin: '0 5%',
-              width: '40%',
-              height: '60%',
-              background: 'aqua',
             }}
           >
-            <h1>Michel</h1>
-          </div>
-        </ParallaxLayer>
-      </Parallax>
-    </div>
+            <img
+              src={require('./imgs/moon-surface-big-planet-background.jpg')}
+              style={{ display: 'block', width: '100%', height: '100%' }}
+            />
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={0.4}
+            speed={-0.2}
+            style={{
+              alignCenter,
+              justifyContent: 'center',
+            }}
+          >
+            <h1 style={{ width: '100%', height: '20%', color: 'white' }}>
+              Welcome aboard tuna lover!
+            </h1>
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={3}
+            speed={-0.2}
+            style={{
+              ...alignCenter,
+              justifyContent: 'center',
+            }}
+          >
+            <Crew dataCrew={dataCrew}></Crew>
+          </ParallaxLayer>
+          {/* <ParallaxLayer
+            sticky={{ start: 1, end: 2.5 }}
+            style={{ alignCenter, justifyContent: 'center' }}
+          >
+            <div
+              className='cards-sticky'
+              style={{
+                background: 'yellow',
+              }}
+            >
+              <h1>Mini moy</h1>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer
+            sticky={{ start: 1.5, end: 2.5 }}
+            style={{ ...alignCenter, justifyContent: 'center', top: '10px' }}
+          >
+            <div
+              className='cards-sticky'
+              style={{
+                background: 'red',
+                top: '10px',
+              }}
+            >
+              <h1>Mini moy</h1>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer
+            sticky={{ start: 2, end: 2.5 }}
+            style={{ ...alignCenter, justifyContent: 'center', top: '20px' }}
+          >
+            <div
+              className='cards-sticky'
+              style={{
+                background: 'brown',
+                top: '20px',
+              }}
+            >
+              <h1>Mini moy</h1>
+            </div>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={3}
+            speed={1}
+            style={{
+              ...alignCenter,
+              justifyContent: 'flex-start',
+            }}
+          >
+            <div
+              className='cards'
+              style={{
+                background: 'orange',
+              }}
+            >
+              <h1>Bernard</h1>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={3}
+            speed={10}
+            style={{
+              ...alignCenter,
+              justifyContent: 'flex-end',
+            }}
+          >
+            <div
+              className='cards'
+              style={{
+                background: 'aqua',
+              }}
+            >
+              <h1>Michel</h1>
+            </div>
+          </ParallaxLayer> */}
+        </Parallax>
+      </div>
+    </>
   );
 }
-
+// <a href="https://www.freepik.com/free-photo/moon-surface-big-planet-background_29302738.htm#&position=0&from_view=collections">Image by wirestock</a> on Freepik
+// <a href="https://www.freepik.com/free-ai-image/one-person-piloting-futuristic-spaceship-outdoors-generated-by-ai_42309548.htm#&position=0&from_view=collections">Image By vecstock</a>
+{
+  /* <a href='https://www.freepik.com/free-ai-image/rocket-flying-through-space_41745942.htm#&position=1&from_view=collections'>
+  Image By freepik
+</a>; */
+}
+// <a href="https://www.freepik.com/free-photo/space-planet-science-night-generated-by-ai_41219020.htm#&position=4&from_view=collections">Image by vecstock</a> on Freepik
+// <a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
+// <a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
+// <a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
+// <a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
 export default App;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const catCrew = [
+  {
+    id: 1,
+    img: require('./imgs/1920-cat-in-astronaut-helmet-portrait-of-a-cat-in-space-ai-generative-image.jpg'),
+    name: 'Flurball',
+    role: 'Pilot',
+    desc: 'Spacecraft pilot expert',
+    funFact: 'Head scratch',
+    like: 'Tuna',
+  },
+  {
+    id: 2,
+    img: require('./imgs/1920-cat-in-white-astronaut-costume-ai-generated.jpeg'),
+    name: 'Flurball',
+    role: 'Capitain',
+    desc: 'Was the first cat to reach Mars',
+    funFact: 'Was the first cat to reach Mars',
+    like: 'Tuna',
+  },
+  {
+    id: 3,
+    img: require('./imgs/1920-ai-generative-a-cat-in-spy-outfit.png'),
+    name: 'Stepan',
+    role: 'Security',
+    desc: 'Always ready for contact',
+    funFact: 'Ex special force',
+    like: 'Tuna',
+  },
+  {
+    id: 4,
+    img: require('./imgs/1920-cat-wearing-astronaut-suit-ai-genertaive.jpg'),
+    name: 'Fripou',
+    role: 'Engineer',
+    desc: 'Graduated with honor from the catstronaut academy',
+    funFact: 'Sleep only 12 hours a day',
+    like: 'Tuna',
+  },
+];
+
+export default catCrew;
