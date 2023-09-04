6d53a6835e7f4e2ac8cb30c5ca56bb0d9dc47581
Kevin Hanard <kevin.hanard@gmail.com>
2023-09-04 15:56:40 +1000

    feat: optimization photos size

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ function App() {
             <img
               className='spacecraft'
               src={require('./imgs/rocket-flying-through-space.jpg')}
+              alt='rocket flying'
+              rel='preload'
             />
           </ParallaxLayer>
           <ParallaxLayer
@@ -54,6 +56,7 @@ function App() {
               <div className='stick'>
                 <img
                   src={require('./imgs/moon-surface-big-planet-background.jpg')}
+                  alt='mission target'
                 ></img>
                 <div className='text'>
                   <p>
@@ -75,7 +78,10 @@ function App() {
             <div className='cards-sticky'>
               <h2>Our Mission</h2>
               <div className='stick'>
-                <img src={require('./imgs/9398462_347251.png')}></img>
+                <img
+                  src={require('./imgs/9398462_347251.png')}
+                  alt='mission description'
+                ></img>
                 <div className='text'>
                   <p>
                     It involves a journey of months, during which they will
@@ -98,6 +104,7 @@ function App() {
               <div className='stick'>
                 <img
                   src={require('./imgs/empty-dark-room-modern-futuristic-sci-fi-background-3d-illustration.jpg')}
+                  alt='storeroom'
                 ></img>
                 <div className='loot'>
                   <p>
@@ -158,25 +165,3 @@ function App() {
 }
 
 export default App;
-
-//<a href="https://www.freepik.com/free-photo/empty-dark-room-modern-futuristic-sci-fi-background-3d-illustration_21988050.htm#query=spacecraft%20store%20room&position=16&from_view=search&track=ais#position=16&query=spacecraft%20store%20room">Image by user6702303</a> on Freepik
-// <a href="https://www.freepik.com/free-photo/moon-surface-big-planet-background_29302738.htm#&position=0&from_view=collections">Image by wirestock</a> on Freepik
-// <a href="https://www.freepik.com/free-ai-image/one-person-piloting-futuristic-spaceship-outdoors-generated-by-ai_42309548.htm#&position=0&from_view=collections">Image By vecstock</a>
-{
-  /* <a href='https://www.freepik.com/free-ai-image/rocket-flying-through-space_41745942.htm#&position=1&from_view=collections'>
-  Image By freepik
-</a>; */
-}
-{
-  /* <a href='https://www.freepik.com/search?format=search&last_filter=type&last_value=icon&query=cat%20paw&selection=1&type=icon'>
-  Icon by Freepik
-</a>; */
-}
-//<a href="https://www.freepik.com/free-vector/abstract-future-interface-composition_9398462.htm#query=spacecraft%20objectif&position=17&from_view=search&track=ais">Image by macrovector</a> on Freepik
-// <a href="https://www.freepik.com/search?format=search&last_filter=type&last_value=icon&query=cat%20world&selection=1&type=icon">Icon by Freepik</a>
-
-// <a href="https://www.freepik.com/free-photo/space-planet-science-night-generated-by-ai_41219020.htm#&position=4&from_view=collections">Image by vecstock</a> on Freepik
-// <a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
-// <a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
-// <a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
-// <a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
